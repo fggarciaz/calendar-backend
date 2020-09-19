9c57b8aaea77208d216374354071e210ef9b1000
Fabricio Garcia <fgarciazerda@gmail.com>
2020-09-19 02:07:44 -0500

    CalendarApp v1

---
+++ public/precache-manifest.500dde8b044237745308f77448bdf1af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c868b5300ed36d1a44b53321a2fff6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "494c12a39849fe3eec3f",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "28faf65132ccff18aa87",
+    "url": "/static/css/main.992c6026.chunk.css"
+  },
+  {
+    "revision": "494c12a39849fe3eec3f",
+    "url": "/static/js/2.b4062b6d.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.b4062b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28faf65132ccff18aa87",
+    "url": "/static/js/main.fc3babfb.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
